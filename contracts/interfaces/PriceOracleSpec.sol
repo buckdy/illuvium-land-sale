13d29bc808e4e6aaf9d9cbae4246ca7627b86e1a
buckdy <buck3dy@gmail.com>
2021-11-10 15:31:40 +0200

    LD-414: include the update() function into the price oracle PairOracle interface

--- contracts/interfaces/PriceOracleSpec.sol
+++ contracts/interfaces/PriceOracleSpec.sol
@@ -7,12 +7,24 @@ pragma solidity 0.8.7;
  * @notice Generic interface used to consult on the Uniswap-like token pairs conversion prices;
  *      one pair oracle is used to consult on the exchange rate within a single token pair
  *
+ * @notice See also: https://docs.uniswap.org/protocol/V2/guides/smart-contract-integration/building-an-oracle
+ *
  * @author Basil Gorin
  */
 interface PairOracle {
 	/**
+	 * @notice Updates the oracle with the price values if required, for example
+	 *      the cumulative price at the start and end of a period, etc.
+	 *
+	 * @dev This function is part of the oracle maintenance flow
+	 */
+	function update() external;
+
+	/**
 	 * @notice For a pair of tokens A/B (sell/buy), consults on the amount of token B to be
 	 *      bought if the specified amount of token A to be sold
+	 *
+	 * @dev This function is part of the oracle usage flow
 	 *
 	 * @param token token A (token to sell) address
 	 * @param amountIn amount of token A to sell
