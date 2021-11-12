fcd2b8925dec3e4a80729df9e2ed1c2f2b150036
buckdy <buck3dy@gmail.com>
2021-11-12 18:01:37 +0200

    LD-414: mock the price oracle

--- contracts/utils/LandSaleOracle.sol
+++ contracts/utils/LandSaleOracle.sol
@@ -24,28 +24,3 @@ interface LandSaleOracle {
 	 */
 	function ethToIlv(uint256 ethOut) external view returns(uint256 ilvIn);
 }
-
-/**
- * @title Land Sale Oracle Implementation
- *
- * @notice Supports the Land Sale with the USD/ETH and USD/ILV conversion required
- *
- * @author Basil Gorin
- */
-contract LandSaleOracleImpl is LandSaleOracle, IERC165 {
-	/**
-	 * @inheritdoc IERC165
-	 */
-	function supportsInterface(bytes4 interfaceID) public view virtual override returns (bool) {
-		// determine and return the interface support
-		return interfaceID == type(LandSaleOracle).interfaceId;
-	}
-
-	/**
-	 * @inheritdoc LandSaleOracle
-	 */
-	function ethToIlv(uint256 ethOut) public view virtual override returns(uint256 ilvIn) {
-		// TODO: implement
-		return ethOut * 4;
-	}
-}
