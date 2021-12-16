3108f0af101b4f0feba7a502aeff33c1e2b14f18
pedrobergamini <pedrohgbergamini@gmail.com>
2021-12-16 19:34:39 -0300

    [LD-482]: description and name

--- contracts/lib/LandDescriptor.sol
+++ contracts/lib/LandDescriptor.sol
@@ -2,13 +2,30 @@
 pragma solidity 0.8.7;
 
 import "base64-sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
 
 library LandDescriptor {
 	using Base64 for bytes;
+	using Strings for uint256;
 
-	function _generateLandName() private pure returns (string memory) {}
+	function _generateLandName(uint8 _regionId, uint16 _x, uint16 _y, uint8 _tierId private pure returns (string memory) {
+		return string(
+			abi.encodePacked(
+				"Land Tier ",
+				uint256(_tierId).toString(),
+				" - (",
+				uint256(_regionId).toString(),
+				", ",
+				uint256(_x).toString(),
+				", ",
+				uint256(_y).toString()
+			);
+		);
+	}
 
-	function _generateLandDescription() private pure returns (string memory) {}
+	function _generateLandDescription() private pure returns (string memory) {
+		return "Describes the asset to which this NFT represents";
+	}
 
 	function _generateSVG() private pure returns (string memory) {}
 
