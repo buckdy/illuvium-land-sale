33949e810ae8c3eb7507ff70d44a1185ff941622
yuriarthf <yuri.fernandes@illuvium.io>
2022-02-22 13:19:11 +0700

    LD-664: skip unnecessary coverage tests for LandSvgLib

--- test/svg_gen/land_svg_lib_test.js
+++ test/svg_gen/land_svg_lib_test.js
@@ -8,6 +8,7 @@ log.setLevel(process.env.LOG_LEVEL? process.env.LOG_LEVEL: "info");
 // Zeppelin test helpers
 const {
 	constants,
+	BN,
 	expectRevert,
 } = require("@openzeppelin/test-helpers");
 const {
@@ -43,29 +44,31 @@ contract("LandDescriptor: Land SVG Generator Tests", function(accounts) {
 	// H0 – initial token holder account
 	const [a0, H0] = accounts;
 
-	// grid (plot) sizes
-	const plot_sizes = [48, 49, 50, 51, 52];
+	// grid (plot) sizes (provide at least one `plot_size` of each parity)
+	// define plot_sizes to be used in coverage and normal runs (both)
+	const plot_sizes_coverage = [48, 49];
+	const plot_sizes = [50, 51, 52];
 
 	// randomize plot sizes
 	const max_plot_size = 120;
 	const rnd_plot_sizes = 5;
 
 	// Define token IDs
-	const tokenIDs = new Array(plot_sizes.length + rnd_plot_sizes).fill(0).map((v, i) => i + 1);
+	const token_ids = new Array(plot_sizes_coverage.length + plot_sizes.length + rnd_plot_sizes).fill(0).map((_, i) => i + 1);
 
 	// Custom generate_land_plots with the plot_sizes option
 	function generate_land_plots(plot_sizes) {
-		const landPlots = [];
+		const land_plots = [];
 		for(const size of plot_sizes) {
-			landPlots.push(generate_land_plot_metadata(undefined, undefined, undefined, [size]));
+			land_plots.push(generate_land_plot_metadata(undefined, undefined, undefined, [size]));
 		}
-		return landPlots;
+		return land_plots;
 	}
 
 	// Extract SVG string from Land Descriptor return data
-	function get_svg_string(token_URI) {
+	function get_svg_string(token_uri) {
 		const svg_base64 = JSON.parse(
-			new Buffer.from(token_URI.split(" ")[1], "base64").toString("ascii"))["image"]
+			new Buffer.from(token_uri.split(" ")[1], "base64").toString("ascii"))["image"]
 			.split(",")[1];
 		return new Buffer.from(svg_base64, "base64").toString("ascii");
 	}
@@ -75,6 +78,10 @@ contract("LandDescriptor: Land SVG Generator Tests", function(accounts) {
 		return gen_random_plot_sizes(undefined, to, n, fixed_plot_sizes);
 	}
 
+	// Generate additional random plot sizes
+	// Generate on the outer scope so it's available for all other scopes
+	const extended_plot_sizes = attach_random_plot_sizes(max_plot_size, rnd_plot_sizes, plot_sizes);
+
 	// deploy LandDescriptor
 	let land_descriptor;
 	// Deploy LandERC721 - Required to test LandDescriptor
@@ -82,18 +89,18 @@ contract("LandDescriptor: Land SVG Generator Tests", function(accounts) {
 	beforeEach(async() => {
 		// Deploy LandDescriptor
 		land_descriptor = await land_descriptor_deploy(a0);
+
 		// Deploy LandERC721
 		land_nft = await land_nft_deploy(a0);
+
 		// set the LandDescriptor implementation
 		await land_nft.setLandDescriptor(land_descriptor.address, {from: a0});
-		// Generate random plot_sizes
-		const extended_plot_sizes = attach_random_plot_sizes(max_plot_size, rnd_plot_sizes, plot_sizes);
+
 		// Generate some land plot NFTs
-		// set the token URI and base URI,
-		// support the tokens Zeppelin is going to mint with some metadata (otherwise it fails)
-		let landPlots = generate_land_plots(extended_plot_sizes)
-		for(let i = 0; i < tokenIDs.length; i++) {
-			await land_nft.mintWithMetadata(H0, tokenIDs[i], landPlots[i], {from: a0});
+		// Mint NFTs on `land_nft` ERC721 contract given metadata
+		let land_plots = generate_land_plots(plot_sizes_coverage.concat(extended_plot_sizes));
+		for(let i = 0; i < token_ids.length; i++) {
+			await land_nft.mintWithMetadata(H0, token_ids[i], land_plots[i], {from: a0});
 		}
 	});
 
@@ -105,67 +112,70 @@ contract("LandDescriptor: Land SVG Generator Tests", function(accounts) {
 			land_svg_lib_mock = await land_svg_lib_mock_deploy(a0);
 		});
 
-		function test_generate_land_name(regionId, x = 0, y = 0) {
-			it(`Generate Land Name for regionId: ${regionId}, x: ${x} and y: ${y}`, async () => {
-				if (regionId >= 1 && regionId <= 7) {
+		function test_generate_land_name(region_id, x = 0, y = 0) {
+			it(`Generate Land Name for regionId: ${region_id}, x: ${x} and y: ${y}`, async () => {
+				if (region_id >= 1 && region_id <= 7) {
 					// Call solidity function
-					outputSol = await land_svg_lib_mock.generateLandName(regionId, x, y);
+					return_data_sol = await land_svg_lib_mock.generateLandName(region_id, x, y);
 
 					// Get JS Implementation output
-					outputJs = generateLandName(regionId, x, y);
+					return_data_js = generateLandName(region_id, x, y);
 
-					expect(outputSol).to.be.equal(outputJs);
+					expect(return_data_sol).to.be.equal(return_data_js);
 				} else {
 					// Call solidity function
-					outputSol = land_svg_lib_mock.generateLandName(regionId, x, y);
+					return_data_sol = land_svg_lib_mock.generateLandName(region_id, x, y);
 
 					// Expect revertion with "Invalid region ID"
-					await expectRevert(outputSol, "Invalid region ID");
+					await expectRevert(return_data_sol, "Invalid region ID");
 				}
 			});
 		}
 
 		// land names with regionId equal to 0 or 8 (or greater) should revert
-		[0, 1, 2, 3, 4, 5, 6, 7, 8].forEach(regionId => test_generate_land_name(regionId));
+		[0, 1, 2, 3, 4, 5, 6, 7, 8].forEach(region_id => test_generate_land_name(region_id));
 	});
 
-	describe(`Generate Land SVGs for token IDs: ${tokenIDs} through LandERC721 contract`, function() {
-
-		function test_token_URI(tokenID) {
-			it(`gen Land SVG file for ${tokenID}`, async function() {
+	describe(`Generate Land SVGs for token IDs: ${token_ids} through LandERC721 contract`, function() {
+		function test_token_URI(token_id, skip_on_coverage = false) {
+			it(`gen Land SVG file for ${token_id}${skip_on_coverage ? " [ @skip-on-coverage ]" : ""}`, async () => {
 				// Estimate gas cost
-				const gas_eta = await land_nft.tokenURI.estimateGas(tokenID, {gas: constants.MAX_UINT256});
-				log.info(`Estimated gas amount for ${tokenID} SVG generation: ${gas_eta}`);
+				const gas_eta = await land_nft.tokenURI.estimateGas(token_id, {gas: constants.MAX_UINT256});
+				log.info(`Estimated gas amount for ${token_id} SVG generation: ${gas_eta}`);
 	
 				// Log Resource sites info
 				if(log.getLevel() <= log.levels.DEBUG) {
-					const resourceSites = plot.sites;
+					const resource_sites = plot.sites;
 					log.debug("Site list:");
-					for(const site of resourceSites) {
+					for(const site of resource_sites) {
 						log.debug(`Resource type: ${site.typeId} (${site.typeId < 4? "element": "fuel"})`);
 						log.debug(`Coordinates: (${site.x}, ${site.y})\n`);
 					}
 				}
 				// Get plot for tokenID and generate SVG using JS impl
-				const plotView = await land_nft.viewMetadata(tokenID);
-				const returnDataJs = LandDescriptor.tokenURI(plotView);
+				const plot_view = await land_nft.viewMetadata(token_id);
+				const return_data_js = LandDescriptor.tokenURI(plot_view);
 	
 				// Print sites to make sure the SVG positioning is correct
-				log.debug(print_plot(plotView));
+				log.debug(print_plot(plot_view));
 	
 				// Get token SVG string from LandERC721
-				const returnData = await land_nft.tokenURI(tokenID, {gas: constants.MAX_UINT256});
+				const return_data_sol = await land_nft.tokenURI(token_id, {gas: constants.MAX_UINT256});
 	
 				// Check if it's equal to the one generated directly from Land Descriptor
-				expect(returnData).to.be.equal(returnDataJs);
+				expect(return_data_sol).to.be.equal(return_data_js);
 	
 				// Generate Land SVG and write to file
-				const path = save_svg_to_file(`land_svg_token_id_${tokenID}_gridsize_${plotView.size}`, get_svg_string(returnData));
+				const path = save_svg_to_file(
+					`land_svg_token_id_${token_id}_gridsize_${plot_view.size}`, get_svg_string(return_data_sol));
 				log.info("SVG saved to %o", path);
 			});
 		}
-
+		
 		// Test Land SVG generation for all token IDs
-		tokenIDs.forEach(test_token_URI);
+		token_ids.slice(0, plot_sizes_coverage.length).forEach(token_id => test_token_URI(token_id));
+
+		// Test Land SVG generation for all token IDs [ @skip-on-coverage ]
+		token_ids.slice(plot_sizes_coverage.length).forEach(token_id => test_token_URI(token_id, true));
 	});
 });
