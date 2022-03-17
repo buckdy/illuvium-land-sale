113a89ca65d40f514495d3fd03b4298fd9c52b03
yuriarthf <yuri.fernandes@illuvium.io>
2022-03-17 20:43:58 +0700

    DEX-500: simplified code and removed contract instantiation

--- scripts/get_access_control_users.js
+++ scripts/get_access_control_users.js
@@ -4,37 +4,48 @@ const axios = require("axios");
 // constants to get the FULL_PRIVILEGE_MASK (MAX_UINT256)
 const { constants } = require("@openzeppelin/test-helpers");
 
-/**
- * @dev instantiate an arbitrary contract which contains the AccessControl ABI
- * 
- * @param contract_address address of the contract
- * @param proxy flag indicating if contract is a proxy or not
- * @return Web3's contract instance
- */
-function get_access_control_contract(contract_address, proxy=true) {
-	// instantiate AccessControl inherited contract
-	return new web3.eth.Contract(
-		artifacts.require(proxy ? "UpgradeableAccessControl" : "AccessControl").abi,
-		contract_address
-	);
-}
+// JSON interface for the RoleUpdated event
+const ROLE_UPDATED_INPUTS = [
+	{
+		type: "address",
+		name: "_by",
+		indexed: true,
+	},
+	{
+		type: "address",
+		name: "_to",
+		indexed: true,
+	},
+	{
+		type: "uint256",
+		name: "_requested"
+	},
+	{
+		type: "uint256",
+		name: "_actual"
+	}
+];
 
 /**
- * @dev get all past events given a contract's web3 instance and filters
+ * @dev get all past RoleUpdated logs of a given contract (AccessControl inherited)
  * 
- * @param web3_contract Web3's contract instance
- * @param event_name name of the event
- * @param filter topic filters
+ * @param access_control_contract_addr address of an AccessControl inherited contract
  * @param from_block the block from to start fetching the events
  * @param to_block the last block to fetch for events
  * @return array of past Event(s)
  */
-function get_contract_events(web3_contract, event_name, filter, from_block, to_block) {
-	return web3_contract.getPastEvents(event_name, {
-		filter,
+function get_role_updated_logs(access_control_contract_addr, from_block = 0, to_block = "latest") {
+	return web3.eth.getPastLogs({
 		fromBlock: from_block,
 		toBlock: to_block,
-	});
+		address: access_control_contract_addr,
+		topics: [
+			"0x5a10526456f5116c0b7b80582c217d666243fd51b6a2d92c8011e601c2462e5f",
+			null,
+			null,
+			null
+		]
+	})
 }
 
 /**
@@ -57,26 +68,30 @@ async function retrieve_transactions(eth_address, network) {
 /**
  * @dev get the user to roles mapping for a given AccessControl inherited contract
  * 
- * @param web3_contract Web3's contract instance
+ * @param access_control_contract_addr address of an AccessControl inherited contract
  * @param network name of the network
  * @return object containing the mapping (user => role)
  */
-async function get_user_roles(web3_contract, network) {
-	// get all RoleUpdated events
-	const role_updated_events = await get_contract_events(web3_contract, "RoleUpdated");
+async function get_user_roles(access_control_contract_addr, network) {
+	// get all RoleUpdated logs
+	const role_updated_logs = await get_role_updated_logs(access_control_contract_addr);
 
-	// get contract creation transaction
-	const contract_creation_tx = (await retrieve_transactions(web3_contract.options.address, network))[0];
+	// get contract creation transaction (to retrieve the deployer -- FULL_PRIVILEGE_MASK)
+	const contract_creation_tx = (await retrieve_transactions(access_control_contract_addr, network))[0];
 	
 	// get latest role change for each address
 	const user_to_role_mapping = {};
 	const user_to_role_updated_mapping = {};
-	let user;
-	for (const event of role_updated_events) {
-		user = event.returnValues._to;
-		if (event.blockNumber < user_to_role_updated_mapping[user]) continue;
-		user_to_role_mapping[user] = event.returnValues._actual;
-		user_to_role_updated_mapping[user] = event.blockNumber;
+	let decoded_log;
+	for (const log of role_updated_logs) {
+		decoded_log = web3.eth.abi.decodeLog(
+			ROLE_UPDATED_INPUTS,
+			role_updated_logs[0].data,
+			role_updated_logs[0].topics.slice(1)
+		);
+		if (log.blockNumber < user_to_role_updated_mapping[decoded_log._to]) continue;
+		user_to_role_mapping[decoded_log._to] = decoded_log._actual;
+		user_to_role_updated_mapping[decoded_log._to] = log.blockNumber;
 	}
 
 	if (!(contract_creation_tx.from in user_to_role_mapping)) {
@@ -90,11 +105,8 @@ async function get_user_roles(web3_contract, network) {
 // all the logic into async main and execute it in the end of the file
 // see https://javascript.plainenglish.io/writing-asynchronous-programs-in-javascript-9a292570b2a6
 async function main() {
-	// get AccessControl inherited contract
-	const contract = await get_access_control_contract(process.env.ACCESS_CONTROL_ADDR);
-
 	// print the latest roles for each user
-	console.log(await get_user_roles(contract, network.name));
+	console.log(await get_user_roles(process.env.ACCESS_CONTROL_ADDR, network.name));
 }
 
 // We recommend this pattern to be able to use async/await everywhere
