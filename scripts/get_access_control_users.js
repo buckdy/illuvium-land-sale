ad093a79888a7e56a0468bedcf77f26e47398882
yuriarthf <yuri.fernandes@illuvium.io>
2022-03-17 19:05:07 +0700

    DEX-500: get contract deployer from etherscan API

--- scripts/get_access_control_users.js
+++ scripts/get_access_control_users.js
@@ -1,3 +1,7 @@
+const axios = require("axios");
+
+const { constants } = require("@openzeppelin/test-helpers");
+
 /**
  * @dev instantiate an arbitrary contract which contains the AccessControl ABI
  * 
@@ -32,14 +36,35 @@ function get_contract_events(web3_contract, event_name, filter, from_block, to_b
 }
 
 /**
+ * @dev retrieve the transactions of an address
+ * 
+ * @param eth_address ethereum address
+ * @param network name of the network
+ * @return `eth_address` transactions for the given network
+ */
+async function retrieve_transactions(eth_address, network) {
+	const network_prefix = network !== "mainnet" ? `-${network}` : "";
+	const response = await axios.get(
+		`https://api${network_prefix}.etherscan.io/api?module=account&action=txlist&address=${eth_address}` + 
+		`&startblock=0&endblock=99999999&sort=asc&apikey=${process.env.ETHERSCAN_KEY}`
+	);
+
+	return response.data.result;
+}
+
+/**
  * @dev get the user to roles mapping for a given AccessControl inherited contract
  * 
  * @param web3_contract Web3's contract instance
+ * @param network name of the network
  * @return object containing the mapping (user => role)
  */
-async function get_user_roles(web3_contract) {
+async function get_user_roles(web3_contract, network) {
 	// get all RoleUpdated events
 	const role_updated_events = await get_contract_events(web3_contract, "RoleUpdated");
+
+	// get contract creation transaction
+	const contract_creation_tx = (await retrieve_transactions(web3_contract.options.address, network))[0];
 	
 	// get latest role change for each address
 	const user_to_role_mapping = {};
@@ -51,6 +76,11 @@ async function get_user_roles(web3_contract) {
 		user_to_role_mapping[user] = event.returnValues._actual;
 		user_to_role_updated_mapping[user] = event.blockNumber;
 	}
+
+	if (!(contract_creation_tx.from in user_to_role_mapping)) {
+		user_to_role_mapping[contract_creation_tx.from] = constants.MAX_UINT256.toString();
+	}
+
 	return user_to_role_mapping;
 }
 
@@ -62,7 +92,7 @@ async function main() {
 	const contract = await get_access_control_contract(process.env.ACCESS_CONTROL_ADDR);
 
 	// print the latest roles for each user
-	console.log(await get_user_roles(contract));
+	console.log(await get_user_roles(contract, network.name));
 }
 
 // We recommend this pattern to be able to use async/await everywhere
